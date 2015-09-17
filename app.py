43b28268e9c9a8967dd28127d7fd0fa214ad1b9d
Helen Bailey <hakbailey@gmail.com>
2015-09-17 16:28:58 -0400

    Fix index page rendering

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ from data import get_dates, process_timeline, process_treemap
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
 @app.route("/timeline")
 def timeline():
 	dates = get_dates('static/dlad_portfolio.json')
 	data = process_timeline('static/dlad_portfolio.json')
 	return render_template("timeline.html", data=data, dates=dates)
 
+@app.route("/")
 @app.route("/treemap")
 def treemap():
 	data = process_treemap('static/dlad_portfolio.json')
